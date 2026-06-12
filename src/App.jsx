4770bdeb7186c0911cdf0cc112a5b0cb34628838
Marina DS <santos.marina.dos@gmail.com>
2026-06-12 11:43:16 +0100

    criaçao dos Routes paths para as paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Register from './pages/Register'
+import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
-
-
   return (
-    <Register />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
